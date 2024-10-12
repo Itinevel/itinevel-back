17abca52b00e16ab60519a61c980c9afcf071fbe
Salim <salimaloui406@gmail.com>
2024-10-12 20:36:35 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,10 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
-  'https://2d32-41-226-167-93.ngrok-free.app',  
+ 'https://2d32-41-226-167-93.ngrok-free.app',  // Your Next.js frontend running locally
   'https://3c05-41-226-167-93.ngrok-free.app',
-  'https://itinevel-front.vercel.app', 
-  'https://itinevel-front-git-main-itinevels-projects.vercel.app', 
+  'https://itinevel-front.vercel.app', // Primary domain
+  'https://itinevel-front-git-main-itinevels-projects.vercel.app', // Main branch domain
   /\.itinevels-projects\.vercel\.app$/
 ];
 
