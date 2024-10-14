47b1c1c211fe7a04c019c48e0c06df3c83c5f5d7
Salim <salimaloui406@gmail.com>
2024-10-14 14:20:13 +0100

    fix

--- api/controllers/testController.ts
+++ api/controllers/testController.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from 'express';
 
 const testRouter = Router();
 
+
 // Define the test route
 testRouter.get('/test', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Success - 200 OK' });
