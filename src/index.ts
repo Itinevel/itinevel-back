789709fd4916d8baae553b9649f65b6694d84900
Salim <salimaloui406@gmail.com>
2024-10-14 10:12:11 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -41,9 +41,15 @@ app.use('/api/users', userRoutes);
 connectMongoDB();
 
 // Start the server
-// const PORT = process.env.PORT || 3001;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+ const PORT = process.env.PORT || 3001;
+ app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+ });
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Hello, world!",
+});
+});
 
 export default app;
